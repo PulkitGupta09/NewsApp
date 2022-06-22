1f0bd55e33c2922d85c3880613a53f46300627ed
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-06-22 13:37:36 +0530

    video 28 completed

--- src/components/News.js
+++ src/components/News.js
@@ -8,18 +8,47 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1 
     };
   }
 
   async componentDidMount(){
     console.log("cdm");
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3cc42736a39a4a7b866f366218920479";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3cc42736a39a4a7b866f366218920479&page=1";
     let data = await fetch(url);
     let parseData = await data.json();  
     console.log(parseData);
-    this.setState({articles: parseData.articles})
+    this.setState({articles: parseData.articles, totalResults: parseData.totalResults})
   }
 
+  handlePrevClick = async ()=>{
+    console.log("clicked on prev click");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3cc42736a39a4a7b866f366218920479&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let parseData = await data.json();  
+    console.log(parseData);
+    this.setState({
+      articles: parseData.articles,
+      page: this.state.page - 1
+
+    })
+  }
+  handleNextClick = async ()=>{
+    if(this.state.page + 1>Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{  
+      console.log("clicked on next click"); 
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3cc42736a39a4a7b866f366218920479&page=${this.state.page + 1}`;
+      let data = await fetch(url);
+      let parseData = await data.json();  
+      console.log(parseData);
+      this.setState({
+        articles: parseData.articles,
+        page: this.state.page + 1
+      }) 
+    }
+  }
   render() {
     return (
       <div className="container">
@@ -29,8 +58,8 @@ export class News extends Component {
             {this.state.articles.map((element)=>{
               return <div className="col-md-4" key = {element.url}>
             <Newsitem
-              title={element.title}
-              description={element.description}
+              title={element.title?element.title:""}
+              description={element.description?element.description:""}
               imageUrl={element.urlToImage?element.urlToImage:"https://ommcom.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/06/21140252/dzzXNZ7G8vDT24RS9EBJKN-e1655800399230.jpg"}
               newsUrl={element.url}
             />
@@ -39,7 +68,14 @@ export class News extends Component {
 
           </div>
         </div>
+        <hr />
+        <div className="container d-flex justify-content-evenly">
+        <button disabled = {this.state.page<=1} type="button" class="btn btn-warning" onClick= {this.handlePrevClick} >&larr; Prev</button>
+        <button disabled = {(this.state.page + 1>Math.ceil(this.state.totalResults/20))}type="button" class="btn btn-warning" onClick= {this.handleNextClick}>Next &rarr;</button>
+        </div>
+        <hr />
       </div>
+      
     );
   }
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -8,9 +8,9 @@ export class navbar extends Component {
     return (
       <>
       <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-success navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Navbar</a>
+          <a className="navbar-brand" href="/">NewsMonkey</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -39,7 +39,7 @@ export class navbar extends Component {
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-light" type="submit">Search</button>
             </form>
           </div>
         </div>
